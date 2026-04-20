3c6659a9e18d4162bd5a89736064feb91f700fd0
amaksma <amaksma@outlook.com>
2026-04-20 23:38:34 +0300

    Remove alternative tree

--- include/expression/derivative.h
+++
@@ -1,9 +0,0 @@
-#ifndef EXPRESSION_DERIVATIVE_H
-#define EXPRESSION_DERIVATIVE_H
-
-#include "expression/node.h"
-
-
-Node *derivative(const Node *root);
-
-#endif
--- include/expression/print_tree.h
+++
@@ -1,9 +0,0 @@
-#ifndef EXPRESSION_PRINT_TREE_H
-#define EXPRESSION_PRINT_TREE_H
-
-#include "expression/node.h"
-
-
-void print_tree(const Node *root);
-
-#endif

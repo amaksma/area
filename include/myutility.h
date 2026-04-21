76fd233335647ba425a9fe6c756c1eb4d0c95f94
amaksma <amaksma@outlook.com>
2026-04-21 23:38:01 +0300

    Add an utility function

--- include/myutility.h
+++ include/myutility.h
@@ -2,11 +2,15 @@
 #define MYUTILITY_H
 
 #include <stddef.h>
+#include <stdint.h>
 
 #define C_PI 3.1415926535897932384626433832795
 #define C_E  2.7182818284590452353602874713527
 
 // Counts the number of occurrences of a character in a string
 size_t count_chars(const char *str, char ch);
+
+// Counts the number of trailing zeros
+int trailing_zeros(uint32_t value);
 
 #endif
--- src/myutility.c
+++ src/myutility.c
@@ -1,4 +1,6 @@
 #include "myutility.h"
+#include <assert.h>
+#include <stdint.h>
 
 inline size_t count_chars(const char *str, char ch) {
     size_t count = 0;
@@ -6,5 +8,17 @@ inline size_t count_chars(const char *str, char ch) {
         if (*str == ch) { ++count; }
         ++str;
     }
+    return count;
+}
+
+int trailing_zeros(uint32_t value) {
+    value &= ~value + 1;
+    int count = 32;
+    if (value != 0) { --count; }
+    if (value & 0x0000FFFF) { count -= 16; }
+    if (value & 0x00FF00FF) { count -= 8; }
+    if (value & 0x0F0F0F0F) { count -= 4; }
+    if (value & 0x33333333) { count -= 2; }
+    if (value & 0x55555555) { count -= 1; }
     return count;
 }

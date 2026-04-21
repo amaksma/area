5004bd6bc646802d3be5a160759301ec3a591c44
amaksma <amaksma@outlook.com>
2026-04-22 02:09:14 +0300

    Add some more utility functions

--- include/myutility.h
+++ include/myutility.h
@@ -7,10 +7,19 @@
 #define C_PI 3.1415926535897932384626433832795
 #define C_E  2.7182818284590452353602874713527
 
+
+typedef double (*func_t)(double);
+
 // Counts the number of occurrences of a character in a string
 size_t count_chars(const char *str, char ch);
 
 // Counts the number of trailing zeros
-int trailing_zeros(uint32_t value);
+unsigned trailing_zeros(uint32_t value);
+
+double abslf(double value);
+double minlf(double a, double b);
+double maxlf(double a, double b);
+double midlf(double a, double b, double c);
+void swaplf(double *a, double *b);
 
 #endif
--- src/myutility.c
+++ src/myutility.c
@@ -11,9 +11,9 @@ inline size_t count_chars(const char *str, char ch) {
     return count;
 }
 
-int trailing_zeros(uint32_t value) {
+unsigned trailing_zeros(uint32_t value) {
     value &= ~value + 1;
-    int count = 32;
+    unsigned count = 32;
     if (value != 0) { --count; }
     if (value & 0x0000FFFF) { count -= 16; }
     if (value & 0x00FF00FF) { count -= 8; }
@@ -21,4 +21,28 @@ int trailing_zeros(uint32_t value) {
     if (value & 0x33333333) { count -= 2; }
     if (value & 0x55555555) { count -= 1; }
     return count;
+}
+
+double abslf(double value) {
+    return value > 0 ? value : -value;
+}
+
+double minlf(double a, double b) {
+    return a < b ? a : b;
+}
+
+double maxlf(double a, double b) {
+    return a > b ? a : b;
+}
+
+double midlf(double a, double b, double c) {
+    if (a > b) { swaplf(&a, &b); }
+    if (a < c) { return minlf(b, c); }
+    return a;
+}
+
+void swaplf(double *a, double *b) {
+    double tmp = *a;
+    *a = *b;
+    *b = tmp;
 }
